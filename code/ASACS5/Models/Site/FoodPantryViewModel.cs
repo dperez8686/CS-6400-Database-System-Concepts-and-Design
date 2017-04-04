4a063e9a9b2033e44cf1977c1ce518619fe760a3
vkv6 <vkv6@gatech.edu>
2017-04-04 06:42:13 -0700

    Added foodpantry details to site

---
+++ code/ASACS5/Models/Site/FoodPantryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASACS5.Models.Site
+{
+	public class FoodPantryViewModel
+	{
+		public int SiteID { get; set; }
+		public string SiteName { get; set; }
+
+		public string StatusType { get; set; }
+		public string StatusMessage { get; set; }
+
+		[DisplayName("Hours of Operation")]
+		[Required(ErrorMessage = "Hours of Operation is required")]
+		[MaxLength(1000, ErrorMessage = "Max length is 1000 characters")]
+		public string HoursOfOperation { get; set; }
+
+		[DisplayName("Conditions For Use")]
+		[MaxLength(1000, ErrorMessage = "Max length is 1000 characters")]
+		public string ConditionsForUse { get; set; }
+
+	}
+}
