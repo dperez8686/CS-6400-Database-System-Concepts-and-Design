346e53d10e8c076672cc07607d896bf827e78eea
Jeff Ross <jross70@gatech.edu>
2017-04-17 23:00:37 -0400

    fix build: add the missing file.

---
+++ Phase 3/ASACS5/Models/Log/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASACS5.Models.Logs
+{
+    public class Log
+    {
+        public int ClientID { get; set; }
+        public int LogID { get; set; }
+        public string DateTimeStamp { get; set; }
+        public string ServiceName { get; set; }
+        public string SiteName { get; set; }
+        public string Description { get; set; }
+    }
+}
